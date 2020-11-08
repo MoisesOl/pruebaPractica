323d6928f93ae01e6d58ca56459b0f1dcfb33e82
MoisesOl <olivaresoviedo99@gmail.com>
2020-11-07 23:11:06 -0300

    Primera conexion a BD y homepage totalmente funcional con BD, mostrando en pantalla las imagenes almacenadas en la BD y pequeño funcionamiento de que si no existe una imagen en el src se muestra una imagen default para que no se vea tan mal.

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Posts;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,16 +18,34 @@ class PostsController extends AbstractController
     /**
      * @Route("/")
      */
-    public function homepage(){
+    public function homepage(EntityManagerInterface $entityManager){
 
-        $datos = (object)[
-            "Id"=>'id 1',
-            "Titulo"=>'Titulo 1',
-            "Descripcion"=>'Descripcion 1',
-            "Imagen"=>'images/stock2.jpg'
-        ];
+        $repository = $entityManager->getRepository(Posts::class);
+        $posts = $repository->findAll();
+        if (!$posts) {
+            throw $this->createNotFoundException(sprintf('Oops!'));
+        }
 
-        return $this->render('posts/homepage.html.twig',[$datos]);
+        return $this->render('posts/homepage.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route ("/posts/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $post = new Posts();
+        $post->setTitulo('Item 3')
+            ->setDescripcion('Description 3')
+            ->setImagen('https://www.google.com');
+        $entityManager->persist($post);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Well hallo! El nuevo post tiene id: #%d',
+            $post->getId()
+        ));
     }
 
     /**
@@ -34,5 +55,12 @@ class PostsController extends AbstractController
         return $this->render('posts/show.html.twig', [
             'titulo' => ucwords(str_replace('-', ' ', $slug))
         ]);
+    }
+
+    /**
+     * @Route ("/login")
+     */
+    public function login(){
+        return $this->render('posts/login.html.twig');
     }
 }
