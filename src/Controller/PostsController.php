54d1763a0669dd235dd66bd4393a6c90c6e2e9ed
MoisesOl <olivaresoviedo99@gmail.com>
2020-11-09 21:25:59 -0300

    Creación de nueva página de subir post con código en progreso para leer imágenes, preparación de nueva librería de doctrine con el objetivo de urlizar la foto que el usuario ingrese

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -9,6 +9,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,23 +37,6 @@ class PostsController extends AbstractController
     }
 
     /**
-     * @Route ("/posts/new")
-     */
-    public function new(EntityManagerInterface $entityManager)
-    {
-        $post = new Posts();
-        $post->setTitulo('Item 4 <i>extra info</i>')
-            ->setDescripcion('Description 4, <i>extra info</i>')
-            ->setImagen('');
-        $entityManager->persist($post);
-        $entityManager->flush();
-        return new Response(sprintf(
-            'Well hallo! El nuevo post tiene id: #%d',
-            $post->getId()
-        ));
-    }
-
-    /**
      * @Route("/posts/usuario/{slug}")
      */
     public function show($slug){
@@ -65,5 +50,61 @@ class PostsController extends AbstractController
      */
     public function login(){
         return $this->render('posts/login.html.twig');
+    }
+
+    /**
+     * @Route ("/upload")
+     */
+    public function upload(EntityManagerInterface $entityManager, Request $request)
+    {
+        return $this->render('posts/upload.html.twig');
+
+
+
+
+        /*$post = new Posts();
+        $post->setTitulo('Item 4 <i>extra info</i>')
+            ->setDescripcion('Description 4, <i>extra info</i>')
+            ->setImagen('');
+        $entityManager->persist($post);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Well hallo! El nuevo post tiene id: #%d',
+            $post->getId()
+        ));*/
+    }
+
+    /**
+     * @Route ("/upload/post")
+     */
+    public function new(Request $request)
+    {
+        /** @var UploadedFile $uploadedFile */
+        $uploadedFile = $request->files->get('image');
+        $destination = $this->getParameter('kernel.project_dir').'/public/images';
+
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = $originalFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+
+        dd($uploadedFile->move(
+            $destination,
+            $newFilename
+        ));
+
+        return $this->render('posts/upload.html.twig');
+
+
+
+
+        /*$post = new Posts();
+        $post->setTitulo('Item 4 <i>extra info</i>')
+            ->setDescripcion('Description 4, <i>extra info</i>')
+            ->setImagen('');
+        $entityManager->persist($post);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Well hallo! El nuevo post tiene id: #%d',
+            $post->getId()
+        ));*/
     }
 }
